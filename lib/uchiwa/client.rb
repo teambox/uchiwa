6f184cf6d62b3b3c55aa05d300e362d2d2a71a43
Andrius Kairiukstis <k@andrius.mobi>
2015-10-02 02:13:08 +0300

    Added logging of HTTP requests besides responses. Started to implement rescuing response headers after exceptions injecting them into the corresponding objects.

--- lib/uchiwa/client.rb
+++ lib/uchiwa/client.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require 'hyperclient'
+require 'logger'
 
 module Uchiwa
   class Client
@@ -14,33 +15,39 @@ module Uchiwa
     module Resource
       module Discover
         def discover
+          log = Logger.new(STDOUT)
+          log.level = Logger::DEBUG
           discoverer = Hyperclient.new('http://lyncdiscover.metio.net') do |client|
             client.connection do |conn|
-              conn.use Faraday::Response::Logger
+              conn.response :logger, log, bodies: true
             end
           end
           begin
             headers = discoverer.user._get.headers
-          rescue => e1
+          rescue Faraday::Error::ClientError => e1
+            discoverer.headers.update('www-authenticate' => e1.response[:headers]['www-authenticate'])
+            headers = e1.response[:headers]
           end
-          puts "get_headers.message = #{e1.response[:message]}"
-          @oauth_url = e1.response[:headers]['www-authenticate'].to_s.match(/href=\"([^\"]*)/)[1]
-          # @oauth_url = headers['www-authenticate'].to_s.match(/href=\"([^\"]*)/)[1]
+
+          # @oauth_url = e1.response[:headers]['www-authenticate'].to_s.match(/href=\"([^\"]*)/)[1]
+          @oauth_url = headers['www-authenticate'].to_s.match(/href=\"([^\"]*)/)[1]
           discoverer.headers.update('Authorization' => "Bearer #{access_token}")
 
-          url = discoverer.user.applications.to_s.sub(/\/ucwa.*/, '')
+          begin
+            url = discoverer.user.applications.to_s.sub(/\/ucwa.*/, '')
+          rescue Faraday::Error::ClientError => e1
+          end
           entry_point = Hyperclient::EntryPoint.new(url)
           entry_point.headers.update('Authorization' => "Bearer #{access_token}")
           begin
             response = discoverer.user.applications._post(application)
-          rescue => e2
+          rescue Faraday::Error::ClientError => e2
           end
-          if e2.response[:success] == 200
+          if e2.response[:status] == 200
             app_resource = Hyperclient::Resource.new(e3.response[:body], entry_point, e2.response)
           else
             app_resource = Hyperclient::Resource.new(nil, entry_point, e2.response)
           end
-          puts "app_resource.response = #{app_resource._response}"
         end
 
         def application
