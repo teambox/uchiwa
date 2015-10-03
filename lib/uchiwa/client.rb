26acf6d806846380c06b9bcde724bb4bbce99cff
Andrius Kairiukstis <k@andrius.mobi>
2015-10-03 10:23:03 +0300

    Finally registered the application at ucwa.skype.com and got the list of resources and links.

--- lib/uchiwa/client.rb
+++ lib/uchiwa/client.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 require 'hyperclient'
 require 'logger'
+require 'json/ext'
 
 module Uchiwa
   class Client
@@ -11,46 +12,45 @@ module Uchiwa
       @config = config
     end
 
-
     module Resource
       module Discover
         def discover
           log = Logger.new(STDOUT)
           log.level = Logger::DEBUG
-          discoverer = Hyperclient.new('http://lyncdiscover.metio.net') do |client|
+          discoverer = Hyperclient.new('https://lyncdiscover.metio.net') do |client|
             client.connection do |conn|
               conn.response :logger, log, bodies: true
             end
           end
+          discoverer.headers.update('Content-Type' =>	'application/json')
+
           begin
             headers = discoverer.user._get.headers
           rescue Faraday::Error::ClientError => e1
-            discoverer.headers.update('www-authenticate' => e1.response[:headers]['www-authenticate'])
             headers = e1.response[:headers]
           end
 
-          # @oauth_url = e1.response[:headers]['www-authenticate'].to_s.match(/href=\"([^\"]*)/)[1]
           @oauth_url = headers['www-authenticate'].to_s.match(/href=\"([^\"]*)/)[1]
-          discoverer.headers.update('Authorization' => "Bearer #{access_token}")
 
-          begin
-            url = discoverer.user.applications.to_s.sub(/\/ucwa.*/, '')
-          rescue Faraday::Error::ClientError => e1
-          end
-          entry_point = Hyperclient::EntryPoint.new(url)
-          entry_point.headers.update('Authorization' => "Bearer #{access_token}")
-          begin
-            response = discoverer.user.applications._post(application)
-          rescue Faraday::Error::ClientError => e2
-          end
-          if e2.response[:status] == 200
-            app_resource = Hyperclient::Resource.new(e3.response[:body], entry_point, e2.response)
-          else
-            app_resource = Hyperclient::Resource.new(nil, entry_point, e2.response)
-          end
+          # begin
+            # discoverer.xframe._post(@oauth_url)
+          # rescue Faraday::Error::ClientError => e10 - get token - not needed in sandbox
+          # end
+
+          discoverer.headers.update('Authorization' => "Bearer #{access_token}")
+          # url = discoverer.user.applications.to_s.sub(/\/ucwa.*/, '')
+          # entry_point = Hyperclient::EntryPoint.new(url)
+          # entry_point.headers.update('Authorization' => "Bearer #{access_token}")
+
+          application = discoverer.user.applications._post(application_id.to_json)
+          # if application._success?
+            # Hyperclient::Resource.new(application, discoverer, application._response)
+          # else
+            # Hyperclient::Resource.new(nil, entry_point, _response)
+          # end
         end
 
-        def application
+        def application_id
           {
             :UserAgent  => "UCWA Samples",
             :EndpointId => SecureRandom.uuid,
