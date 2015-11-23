1d39a41480763712e623bc0dd511c7d069c609ab
Andrius Kairiukstis <k@andrius.mobi>
2015-11-23 13:06:35 +0200

    Code cleanup

--- lib/uchiwa/client.rb
+++ lib/uchiwa/client.rb
@@ -17,8 +17,8 @@ module Uchiwa
 
     def initialize(username, password, domain)
       @logger = ::Logger.new(STDOUT, 'daily')
-      @logger.attach("../logs/#{@name}_client.log")
-      Celluloid.logger = @logger
+      logger.attach("../logs/client.log")
+      Celluloid.logger = logger
 
       @username = username
       @password = password
@@ -30,12 +30,14 @@ module Uchiwa
 
     private
 
-    attr_accessor :username, :password, :domain, :oauth_endpoint, :ucwa_entrance
+    attr_accessor :username, :password, :domain, :oauth_endpoint, :ucwa_entrance, :entry_point,
+                  :logger, :application, :event_handler, :event_channel
 
     module Application
       def auto_discover(domain)
         discoverer = Hyperclient.new("http://lyncdiscover.#{domain}")
-        discoverer.connection.builder.insert_before discoverer.connection.builder.handlers.length - 1, Faraday::Response::Logger, @logger, bodies: true
+        discoverer.connection.builder.insert_before discoverer.connection.builder.handlers.length - 1,
+                                                    Faraday::Response::Logger, @logger, bodies: true
 
         begin
           discoverer.user._get.headers
@@ -43,9 +45,9 @@ module Uchiwa
           oauth_url = e1.response[:headers]['www-authenticate'].to_s.match(/href="([^"]*)/)[1]
         end
 
-        @xframe_url = discoverer._links[:xframe] # TODO: should've been removed
         @oauth_endpoint = Hyperclient::EntryPoint.new(oauth_url)
-        oauth_endpoint.connection.builder.insert_before oauth_endpoint.connection.builder.handlers.length - 1, Faraday::Response::Logger, @logger, bodies: true
+        oauth_endpoint.connection.builder.insert_before oauth_endpoint.connection.builder.handlers.length - 1,
+                                                        Faraday::Response::Logger, logger, bodies: true
         oauth_endpoint.headers.update('Content-Type' =>	'application/x-www-form-urlencoded')
         authenticate
         Uchiwa.set_headers(discoverer, oauth_endpoint._attributes['access_token'], discoverer._links[:xframe])
@@ -57,12 +59,13 @@ module Uchiwa
       end
 
       def register_application
-        @entry_point_url = @ucwa_entrance.user.applications.to_s.sub(/\/ucwa.*/, '') # TODO: not instance variable
-        @entry_point = Hyperclient::EntryPoint.new(@entry_point_url)
-        @entry_point.connection.builder.insert_before @entry_point.connection.builder.handlers.length - 1, Faraday::Response::Logger, @logger, bodies: true
-        Uchiwa.set_headers(@entry_point, oauth_endpoint._attributes['access_token'], ucwa_entrance._links[:xframe])
-        response = @ucwa_entrance.user.applications._post(application_id.to_json)
-        Hyperclient::Resource.new(response._response.body, @entry_point, response._response)
+        entry_point_url = ucwa_entrance.user.applications.to_s.sub(/\/ucwa.*/, '')
+        @entry_point = Hyperclient::EntryPoint.new(entry_point_url)
+        entry_point.connection.builder.insert_before entry_point.connection.builder.handlers.length - 1,
+                                                     Faraday::Response::Logger, logger, bodies: true
+        Uchiwa.set_headers(entry_point, oauth_endpoint._attributes['access_token'], ucwa_entrance._links[:xframe])
+        response = ucwa_entrance.user.applications._post(application_id.to_json)
+        Hyperclient::Resource.new(response._response.body, entry_point, response._response)
       end
 
       def application_id
@@ -74,20 +77,21 @@ module Uchiwa
       end
 
       def set_application
-        @event_channel_url = @application.events._url
         @activity_timer = every(ENV['ACTIVITY_TIMEOUT'].to_i) do
-          @application.me.reportMyActivity._post('')
-          @logger.info 'reportMyActivity request sent'
+          application.me.reportMyActivity._post('')
+          logger.info 'reportMyActivity request sent'
         end
 
-        @event_handler = EventHandler.new(@entry_point)
-        @event_channel = EventChannel.new(@event_channel_url, oauth_endpoint._attributes['access_token'], @entry_point._url, ucwa_entrance._links[:xframe])
-        @scheduler = Scheduler.new(@event_handler, @event_channel)
+        @event_handler = EventHandler.new(entry_point)
+        @event_channel = EventChannel.new(application.events._url,
+                                          oauth_endpoint._attributes['access_token'],
+                                          entry_point._url, ucwa_entrance._links[:xframe])
+        @scheduler = Scheduler.new(event_handler, event_channel)
         @searcher = search_resource
       end
 
       def application_refresh
-        @application = @application.itself._get
+        application.itself._get
       end
     end
 
